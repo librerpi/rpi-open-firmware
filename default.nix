db56d860cf9c406bb39a01537b3d706646f6ebe5
Michael Bishop <cleverca22@gmail.com>
2019-11-30 20:26:34 -0400

    fix names, pre-build uboot

--- default.nix
+++ default.nix
@@ -3,8 +3,9 @@ let
   pkgs = import (builtins.fetchTarball https://github.com/input-output-hk/nixpkgs/archive/0ee0489d42e.tar.gz) {};
   vc4 = pkgs.pkgsCross.vc4;
   arm = pkgs.pkgsCross.arm-embedded;
+  aarch64 = pkgs.pkgsCross.aarch64-multiplatform;
   arm_chainloader = arm.stdenv.mkDerivation {
-    name = "name";
+    name = "chainloader";
     src = ./.;
     postUnpack = ''
       sourceRoot=$sourceRoot/arm_chainloader
@@ -15,7 +16,7 @@ let
     '';
   };
   firmware = vc4.stdenv.mkDerivation {
-    name = "name";
+    name = "firmware";
     src = ./.;
     preBuild = ''
       ln -sv ${arm_chainloader} arm_chainloader/build
@@ -27,4 +28,5 @@ let
   };
 in {
   inherit arm_chainloader firmware;
+  uboot = aarch64.ubootRaspberryPi3_64bit;
 }
