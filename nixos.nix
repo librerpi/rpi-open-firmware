d2225c9dc0a14d5d11a997a279d369eccec9838b
Michael Bishop <michael.bishop@iohk.io>
2020-01-24 01:40:01 -0400

    build nixos natively instead

--- nixos.nix
+++ nixos.nix
@@ -6,14 +6,31 @@ in {
   imports = [
     (sources.nixpkgs + "/nixos/modules/profiles/minimal.nix")
   ];
-  nixpkgs.crossSystem.system = "armv7l-linux";
+  nixpkgs.system = "armv7l-linux";
   fileSystems = {
     "/" = {
       device = "/dev/sda1";
     };
   };
-  boot.loader = {
-    grub.enable = false;
+  boot = {
+    loader = {
+      grub.enable = false;
+    };
+    initrd = {
+      extraUtilsCommands = ''
+        copy_bin_and_libs ${pkgs.strace}/bin/strace
+        cp ${pkgs.stdenv.cc.cc}/armv7l-unknown-linux-gnueabihf/lib/libgcc_s.so $out/lib/ -v
+      '';
+      network = {
+        enable = true;
+        ssh = {
+          enable = true;
+          authorizedKeys = [
+            "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC34wZQFEOGkA5b0Z6maE3aKy/ix1MiK1D0Qmg4E9skAA57yKtWYzjA23r5OCF4Nhlj1CuYd6P1sEI/fMnxf+KkqqgW3ZoZ0+pQu4Bd8Ymi3OkkQX9kiq2coD3AFI6JytC6uBi6FaZQT5fG59DbXhxO5YpZlym8ps1obyCBX0hyKntD18RgHNaNM+jkQOhQ5OoxKsBEobxQOEdjIowl2QeEHb99n45sFr53NFqk3UCz0Y7ZMf1hSFQPuuEC/wExzBBJ1Wl7E1LlNA4p9O3qJUSadGZS4e5nSLqMnbQWv2icQS/7J8IwY0M8r1MsL8mdnlXHUofPlG1r4mtovQ2myzOx clever@nixos"
+          ];
+        };
+      };
+    };
   };
   fonts.fontconfig.enable = false;
   security.polkit.enable = false;
