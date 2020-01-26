7bd7dba56e1690888d4ffe744ed996c617a5eda3
Michael Bishop <michael.bishop@iohk.io>
2020-01-26 01:18:09 -0400

    symlink gdb and valgrind into the initrd

--- nixos.nix
+++ nixos.nix
@@ -34,7 +34,8 @@ in {
     initrd = {
       extraUtilsCommands = ''
         copy_bin_and_libs ${pkgs.strace}/bin/strace
-        copy_bin_and_libs ${pkgs.gdb}/bin/gdb
+        ln -sv ${pkgs.gdb}/bin/gdb $out/bin/gdb
+        ln -sv ${pkgs.valgrind}/bin/valgrind $out/bin/valgrind
         #cp {pkgs.stdenv.cc.cc}/armv7l-unknown-linux-gnueabihf/lib/libgcc_s.so $out/lib/ -v
       '';
       network = {
