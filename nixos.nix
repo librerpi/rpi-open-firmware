4d3a1907a464f13625a5e525aaccc4a31df85c50
Michael Bishop <michael.bishop@iohk.io>
2020-01-27 00:35:19 -0400

    go back to cross-compile, begin adding iscsi

--- nixos.nix
+++ nixos.nix
@@ -1,27 +1,38 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 
 let
   sources = import ./nix/sources.nix;
+  overlay2 = self: super: {
+    e2fsprogs = super.e2fsprogs.overrideDerivation (old: {
+      doCheck = false;
+    });
+    p11-kit = super.p11-kit.overrideDerivation (old: {
+      doCheck = false;
+    });
+  };
 in {
   imports = [
     (sources.nixpkgs + "/nixos/modules/profiles/minimal.nix")
   ];
   nixpkgs = {
-    system = "armv7l-linux";
+    crossSystem.system = "armv7l-linux";
     overlays = [
       (self: super: {
         p11-kit = super.p11-kit.overrideDerivation (old: {
-          doCheck = false;
-        });
-        e2fsprogs = super.e2fsprogs.overrideDerivation (old: {
-          doCheck = false;
+          #doCheck = false;
         });
         coreutils = super.coreutils.overrideDerivation (old: {
-          doCheck = false;
+          #doCheck = false;
         });
+        systemd = super.systemd.overrideDerivation (old: {
+          patches = old.patches ++ [ ./systemd.patch ];
+        });
+        utillinux = super.utillinux.override { systemd = super.systemd; };
       })
     ];
   };
+  networking.useDHCP = false;
+  networking.interfaces.eth0.useDHCP = true;
   fileSystems = {
     "/" = {
       device = "/dev/sda1";
@@ -33,8 +44,10 @@ in {
     };
     initrd = {
       extraUtilsCommands = ''
-        copy_bin_and_libs ${pkgs.strace}/bin/strace
-        ln -sv ${pkgs.valgrind}/bin/valgrind $out/bin/valgrind
+        #ln -sv {pkgs.valgrind}/bin/valgrind $out/bin/valgrind
+        #ln -sv {pkgs.strace}/bin/strace $out/bin/strace
+        # cp {pkgs.stdenv.cc.cc}/armv7l-unknown-linux-gnueabihf/lib/libgcc_s.so $out/lib/ -v
+        copy_bin_and_libs ${(import <nixpkgs> { system = "armv7l-linux"; overlays = [ overlay2 ]; }).openiscsi}/bin/iscsistart
       '';
       network = {
         enable = true;
@@ -44,6 +57,9 @@ in {
             "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC34wZQFEOGkA5b0Z6maE3aKy/ix1MiK1D0Qmg4E9skAA57yKtWYzjA23r5OCF4Nhlj1CuYd6P1sEI/fMnxf+KkqqgW3ZoZ0+pQu4Bd8Ymi3OkkQX9kiq2coD3AFI6JytC6uBi6FaZQT5fG59DbXhxO5YpZlym8ps1obyCBX0hyKntD18RgHNaNM+jkQOhQ5OoxKsBEobxQOEdjIowl2QeEHb99n45sFr53NFqk3UCz0Y7ZMf1hSFQPuuEC/wExzBBJ1Wl7E1LlNA4p9O3qJUSadGZS4e5nSLqMnbQWv2icQS/7J8IwY0M8r1MsL8mdnlXHUofPlG1r4mtovQ2myzOx clever@nixos"
           ];
         };
+        postCommands = ''
+          iscsistart -t iqn.2020-01.com.angeldsis:rpi-image1.img -a 192.168.2.61 -i iqn.2015-09.com.example:3255a7223b2 -g 0
+        '';
       };
     };
   };
