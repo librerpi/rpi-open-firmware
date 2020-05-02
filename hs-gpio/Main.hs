5b630c62e7c4c184c5d7a6171158c160083b461b
Michael Bishop <cleverca22@gmail.com>
2020-05-02 17:37:56 -0300

    add more example of the problem

--- hs-gpio/Main.hs
+++ hs-gpio/Main.hs
@@ -20,10 +20,18 @@ openGPIO :: IO (Ptr (), Fd)
 openGPIO = do
   fd <- openFd "/dev/gpiomem" ReadWrite Nothing defaultFileFlags
   ptr <- c_mmap nullPtr 0x1800000 (protRead <> protWrite) (mkMmapFlags mapShared mempty) fd 0
-  res <- mmap_helper nullPtr 0x1800000 (protRead <> protWrite) (mkMmapFlags mapShared mempty) fd 0
+  res <- mmap_helper 1 2 3 4 5 0x1234567890
+  res <- mmap_helper6 1 2 3 4 5 0x1234567890
+  res <- mmap_helper5 1 2 3 4 0x12345678
   print res
   -- mmap2(NULL, 25165824, PROT_READ|PROT_WRITE, MAP_SHARED, 3, 0) =
   pure (ptr, fd)
 
 foreign import ccall unsafe "mmap_helper"
-  mmap_helper :: Ptr () -> CSize -> ProtOption -> MmapFlags -> Fd -> COff -> IO (Ptr ())
+  mmap_helper :: CInt -> CInt -> CInt -> CInt -> CInt -> COff -> IO (Ptr ())
+
+foreign import ccall unsafe "mmap_helper6"
+  mmap_helper6 :: CInt -> CInt -> CInt -> CInt -> CInt -> COff -> IO (Ptr ())
+
+foreign import ccall unsafe "mmap_helper5"
+  mmap_helper5 :: CInt -> CInt -> CInt -> CInt -> COff -> IO (Ptr ())
--- hs-gpio/cbits/helper.c
+++ hs-gpio/cbits/helper.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int mmap_helper(void *addr, size_t len, int prot, int flags, int fildes, off_t off) {
-  printf("mmap(%p, %x, %d, %d, %d, %ld)\n", addr, len, prot, flags, fildes, off);
+int mmap_helper(int a, int b, int c, int d, int e, off_t off) {
+  printf("mmap(%x, %x, %d, %d, %d, %lx)\n", a, b, c, d, e, off);
+  printf("sizeof(off_t) == %d\n", sizeof(off_t));
+  return 42;
+}
+int mmap_helper6(int a, int b, int c, int d, int e, off_t f) {
+  printf("mmap6(%x, %x, %d, %d, %d, %lx)\n", a, b, c, d, e, f);
+  return 42;
+}
+int mmap_helper5(int a, int b, int c, int d, off_t e) {
+  printf("mmap5(%x, %x, %d, %d, %lx)\n", a, b, c, d, e);
   return 42;
 }
