1bd170d8238550a508c4bc4f99900fb2673e0b39
Michael Bishop <cleverca22@gmail.com>
2020-09-13 02:51:27 -0300

    implement xmodem transmit

--- uart-manager/uart-manager.cpp
+++ uart-manager/uart-manager.cpp
@@ -1,16 +1,22 @@
 //#include <stropts.h>
-#include <unistd.h>
-#include <sys/stat.h>
+#include <cstring>
 #include <fcntl.h>
-#include <termios.h>
-#include <sys/ioctl.h>
-#include <stdio.h>
-#include <sys/epoll.h>
 #include <signal.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string>
+#include <sys/epoll.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 #include <sys/signalfd.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <thread>
+#include <unistd.h>
 
 #define MAX_EVENTS 10
+
+using namespace std;
 
 void monitor_fd(int epollfd, int fd, const char *errmsg) {
   struct epoll_event ev;
@@ -22,6 +28,58 @@ void monitor_fd(int epollfd, int fd, const char *errmsg) {
   }
 }
 
+struct xmodem_packet {
+  uint8_t magic;
+  uint8_t block_num;
+  uint8_t block_num_invert;
+  uint8_t payload[128];
+  uint8_t checksum;
+} __attribute__((packed));
+
+struct send_args {
+  int fd;
+  struct xmodem_packet packet;
+};
+
+static ssize_t write_capped(int fd, void *buf, size_t len) {
+#define CAP 256
+  if (len > CAP) len = CAP;
+  ssize_t ret = write(fd, buf, len);
+  struct timespec t = { 0, 1000 * 1000 * 100 };
+  nanosleep(&t, NULL);
+  return ret;
+}
+
+static void send_packet(struct send_args arg) {
+  struct timespec t;
+  uint8_t *raw_packet = reinterpret_cast<uint8_t*>(&arg.packet);
+  int i = 0;
+  while (i < 132) {
+    //printf("L %d/132", i);
+    //fflush(stdout);
+    int ret = write(arg.fd, raw_packet + i, 132 - i);
+    i += ret;
+  }
+  //printf("sent %d %d %d %d\n", arg.packet.magic, arg.packet.block_num, arg.packet.block_num_invert, arg.packet.checksum);
+}
+
+static void xmodem_send_block(int fd, uint32_t blockNr, void *buffer, int elf_size) {
+  struct xmodem_packet packet;
+
+  packet.magic = 1;
+  packet.block_num = blockNr & 0xff;
+  packet.block_num_invert = 255 - (blockNr & 0xff);
+  memcpy(packet.payload, buffer + (128* (blockNr-1)), 128);
+  uint8_t crc8 = 0;
+  for (int i=0; i<128; i++) {
+    crc8 += packet.payload[i];
+  }
+  packet.checksum = crc8;
+  struct send_args arg = { fd, packet };
+  thread t1(send_packet, arg);
+  t1.detach();
+}
+
 int main(int argc, char **argv) {
   struct epoll_event events[MAX_EVENTS];
   char buffer[512];
@@ -30,6 +88,30 @@ int main(int argc, char **argv) {
   const int flags = TIOCM_DTR;
   sigset_t sigmask;
   struct termios old_tio, new_tio;
+  void *elf_buffer = NULL;
+  int elf_size = 0;
+  bool xmodem = false;
+  int blockNr = 0;
+  int ret;
+
+  string ring_buffer = "";
+
+  if (argc == 2) {
+    int elf_fd = open(argv[1], O_RDONLY);
+    if (elf_fd < 0) {
+      perror("cant open elf file");
+      return 10;
+    }
+    struct stat stats;
+    fstat(elf_fd, &stats);
+    elf_size = stats.st_size;
+    elf_buffer = mmap(NULL, stats.st_size, PROT_READ, MAP_SHARED, elf_fd, 0);
+    if (elf_buffer == MAP_FAILED) {
+      perror("cant mmap elf file");
+      return 11;
+    }
+    close(elf_fd);
+  }
 
   sigemptyset(&sigmask);
   sigaddset(&sigmask, SIGINT);
@@ -54,13 +136,20 @@ int main(int argc, char **argv) {
   options.c_oflag &= ~OPOST;
 
   options.c_cflag &= ~HUPCL;
-  tcsetattr(fd, TCSANOW, &options);
-
+  ret = tcsetattr(fd, TCSANOW, &options);
+  if (ret) {
+    perror("cant do 1st tcsetattr");
+    return 12;
+  }
 
   tcgetattr(STDIN_FILENO,&old_tio);
   new_tio=old_tio;
   new_tio.c_lflag &=(~ICANON & ~ECHO);
-  tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+  ret = tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+  if (ret) {
+    perror("cant do 2nd tcsetattr");
+    return 13;
+  }
 
   epollfd = epoll_create1(0);
   if (epollfd < 0) {
@@ -72,7 +161,11 @@ int main(int argc, char **argv) {
   monitor_fd(epollfd, STDIN_FILENO, "epoll add stdin failed");
   monitor_fd(epollfd, signals, "epoll add signalfd failed");
 
-  ioctl(fd, TIOCMBIC, &flags);
+  ret = ioctl(fd, TIOCMBIC, &flags);
+  if (ret) {
+    perror("cant do 1st ioctl");
+    return 14;
+  }
   bool run = true;
   while (run) {
     int nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
@@ -92,6 +185,8 @@ int main(int argc, char **argv) {
           perror("cant write to stdout");
           return 7;
         }
+        string chunk(buffer, size);
+        ring_buffer += chunk;
       } else if (events[i].data.fd == STDIN_FILENO) {
         size = read(STDIN_FILENO, buffer, 512);
         if (size < 0) {
@@ -103,13 +198,49 @@ int main(int argc, char **argv) {
           return 9;
         }
       } else if (events[i].data.fd == signals) {
-        ioctl(fd, TIOCMBIS, &flags);
+        ret = ioctl(fd, TIOCMBIS, &flags);
+        if (ret) {
+          perror("cant do 2nd ioctl");
+          return 15;
+        }
         run = false;
+      }
+
+      if (ring_buffer.length() > 1024) {
+        ring_buffer = ring_buffer.substr(ring_buffer.length() - 1024, 1024);
+      }
+      if (ring_buffer.find("press X to stop autoboot") != string::npos) {
+        ring_buffer = "";
+        write(fd, "X", 1);
+        xmodem = true;
+        blockNr = 1;
+      }
+      if (xmodem) {
+        if (ring_buffer.find("\x15", 0, 1) != string::npos) {
+          puts("NAK");
+          xmodem_send_block(fd, blockNr, elf_buffer, elf_size);
+          ring_buffer = "";
+        } else if (ring_buffer.find("\6", 0, 1) != string::npos) {
+          printf("ACK %d / %d\r", blockNr, elf_size/128);
+          fflush(stdout);
+          blockNr++;
+          if ((blockNr*128) > elf_size) {
+            write(fd, "\4", 1);
+            puts("EOF!");
+          } else {
+            xmodem_send_block(fd, blockNr, elf_buffer, elf_size);
+          }
+          ring_buffer = "";
+        }
       }
     }
   }
   puts("closing");
   close(fd);
-  tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
+  ret = tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
+  if (ret) {
+    perror("cant do 3rd tcsetattr");
+    return 16;
+  }
   return 0;
 }
