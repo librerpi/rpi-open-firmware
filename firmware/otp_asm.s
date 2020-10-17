6be45466e0be437a1b0b3512a86f3d9627217006
Michael Bishop <cleverca22@gmail.com>
2020-10-16 21:24:18 -0300

    fix assembly filenames

--- firmware/otp_asm.s
+++ firmware/otp_asm.s
@@ -1,32 +1,36 @@
-// copied from https://github.com/ptesarik/vc4boot
+# 1 "otp_asm.S"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "otp_asm.S"
+
 
 A2W_BASE = 0x7e102000
-A2W_XOSC0         = 0x090
+A2W_XOSC0 = 0x090
 
 OTP_BASE = 0x7e20f000
-OTP_CONFIG_REG          = 0x04
-OTP_CTRL_LO_REG         = 0x08
-OTP_CTRL_HI_REG         = 0x0c
-OTP_STATUS_REG          = 0x10
-OTP_DATA_REG            = 0x18
-OTP_ADDR_REG            = 0x1c
+OTP_CONFIG_REG = 0x04
+OTP_CTRL_LO_REG = 0x08
+OTP_CTRL_HI_REG = 0x0c
+OTP_STATUS_REG = 0x10
+OTP_DATA_REG = 0x18
+OTP_ADDR_REG = 0x1c
 
-#if RPI4
-  OTP_READY_BIT = 1
-#else
+
+
+
   OTP_READY_BIT = 0
-#endif
+
 
 .text
 .global otp_read_internal
 otp_read_internal:
   stm r6-r7,lr,(--sp)
 
-  mov r6, r0 // save arg0 in r6
+  mov r6, r0
   bl otp_open
   mov r0, r6
   bl otp_read_reg
-  mov r7, r0 // set the result asside so close wont clobber
+  mov r7, r0
   bl otp_close
   mov r0, r7
 
@@ -37,61 +41,61 @@ otp_open:
   mov r2, 0x03
   mov r1, OTP_BASE
   mov r3, A2W_BASE + A2W_XOSC0
-  st r2, (r1 + OTP_CONFIG_REG) // OTP_CONFIG_REG = 0x3
+  st r2, (r1 + OTP_CONFIG_REG)
 
   mov r0, 2
 1:
-  ld r2, (r3) // read A2W_XOSC0
-  addcmpbge r0, -1, 0, 1b // repeat 2 times total
+  ld r2, (r3)
+  addcmpbge r0, -1, 0, 1b
 
   mov r2, 0
-  st r2, (r1 + OTP_CTRL_HI_REG) // OTP_CTRL_HI_REG = 0
-  st r2, (r1 + OTP_CTRL_LO_REG) // OTP_CTRL_LO_REG = 0
+  st r2, (r1 + OTP_CTRL_HI_REG)
+  st r2, (r1 + OTP_CTRL_LO_REG)
 
   mov r0, 2
 1:
-  ld r2, (r3) // read A2W_XOSC0
-  addcmpbge r0, -1, 0, 1b // repeat 2 times total
+  ld r2, (r3)
+  addcmpbge r0, -1, 0, 1b
 
   mov r2, 0x2
-  st r2, (r1 + OTP_CONFIG_REG) // OTP_CONFIG_REG = 2
+  st r2, (r1 + OTP_CONFIG_REG)
   ldm pc, (sp++)
 
 otp_read_reg:
   stm lr, (--sp)
   mov r1, OTP_BASE
   mov r3, A2W_BASE + A2W_XOSC0
-  st r0, (r1 + OTP_ADDR_REG) // OTP_ADDR_REG = arg0
+  st r0, (r1 + OTP_ADDR_REG)
 
   mov r0, 2
 1:
   ld r2, (r3)
-  addcmpbge r0, -1, 0, 1b // repeat 2 times total
+  addcmpbge r0, -1, 0, 1b
 
   mov r2, 0
-  ld r0, (r1 + OTP_ADDR_REG) // r0 = OTP_ADDR_REG
-  st r2, (r1 + OTP_CTRL_HI_REG) // OTP_CTRL_HI_REG = 0
-  st r2, (r1 + OTP_CTRL_LO_REG) // OTP_CTRL_LO_REG = 0
+  ld r0, (r1 + OTP_ADDR_REG)
+  st r2, (r1 + OTP_CTRL_HI_REG)
+  st r2, (r1 + OTP_CTRL_LO_REG)
 
   mov r0, 2
 1:
   ld r2, (r3)
-  addcmpbge r0, -1, 0, 1b // repeat 2 times total
+  addcmpbge r0, -1, 0, 1b
 
   mov r2, 1
-  ld r0, (r1 + OTP_CTRL_LO_REG) // read ctrl low
-  st r2, (r1 + OTP_CTRL_LO_REG) // ctrl low = 1
-  ld r0, (r1 + OTP_CTRL_LO_REG) // read ctrl low again
+  ld r0, (r1 + OTP_CTRL_LO_REG)
+  st r2, (r1 + OTP_CTRL_LO_REG)
+  ld r0, (r1 + OTP_CTRL_LO_REG)
 1:
   mov r0, 2
 2:
   ld r2, (r3)
-  addcmpbge r0, -1, 0, 2b // repeat 2 times total
+  addcmpbge r0, -1, 0, 2b
 
-  ld r0, (r1 + OTP_STATUS_REG)  // read status reg
-  btest r0, OTP_READY_BIT       // check if its ready
-  beq 1b                        // if not ready, stall some more and try again
-  ld r0, (r1 + OTP_DATA_REG)    // read final answer
+  ld r0, (r1 + OTP_STATUS_REG)
+  btest r0, OTP_READY_BIT
+  beq 1b
+  ld r0, (r1 + OTP_DATA_REG)
   ldm pc, (sp++)
 
 otp_close:
@@ -99,13 +103,13 @@ otp_close:
   mov r2, 0
   mov r1, OTP_BASE
   mov r3, A2W_BASE + A2W_XOSC0
-  st r2, (r1 + OTP_CTRL_HI_REG) // OTP_CTRL_HI_REG = 0
-  st r2, (r1 + OTP_CTRL_LO_REG) // OTP_CTRL_LO_REG = 0
+  st r2, (r1 + OTP_CTRL_HI_REG)
+  st r2, (r1 + OTP_CTRL_LO_REG)
 
   mov r0, 2
 1:
   ld r2, (r3)
-  addcmpbge r0, -1, 0, 1b // repeat 2 times total
+  addcmpbge r0, -1, 0, 1b
 
   mov r2, 0
   st r2, (r1 + OTP_CONFIG_REG)
